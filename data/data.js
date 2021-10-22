124526b6e8f4cdc8f4153b3d6aec147d024de229
Thomas <thomas.s.casco@gmail.com>
2021-10-22 17:48:11 -0300

    all the code

---
+++ data/data.js
@@ -0,0 +1,47 @@
+export const data = [
+  {
+    question: "Cual de las siguientes sneakers no fueron creadas por Nike?",
+    choices: [
+      "Air Force one",
+      "Blazer Mid 77 White Indigo",
+      "Dunk High Championship Navy",
+      "Yeezy Slide Pure",
+    ],
+    answer: "Yeezy Slide Pure",
+  },
+
+  {
+    question: "Para que reconocida marca trabaja Sean Whoterspoon?",
+    choices: ["Nike", "Puma", "Adidas", "Gucci"],
+    answer: "Adidas",
+  },
+
+  {
+    question: "Cual fue el modelo de la colaboracion Adidas X Dior?",
+    choices: [
+      "Adidas Ozweego",
+      "Adidas Superstar",
+      "Adidas NMD",
+      "Adidas Stan Smith",
+    ],
+    answer: "Adidas Superstar",
+  },
+
+  {
+    question: "Quien fue el creador de las Air Force One?",
+    choices: ["Bruce Kilgore", "Tinker Hatfield", "Chitose Abe", "Peter Moore"],
+    answer: "Bruce Kilgore",
+  },
+
+  {
+    question: "Cual de los siguientes modelos NO llegaron a argentina?",
+    choices: [
+      "Nike Vapormax",
+      "Air Force 1 Travis Scott",
+      "Nike x Stussy Air Force 1",
+      "Yeezy 750 v2 Static",
+      "SB Dunk Low Travis Scott",
+    ],
+    answer: "Nike x Stussy Air Force 1",
+  },
+];
